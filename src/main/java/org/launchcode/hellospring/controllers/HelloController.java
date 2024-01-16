b6c2bf182cca020ebae1b2b81753fdf0657a8a27
EzekielCz <ezechielcazacu@gmail.com>
2024-01-15 22:24:14 -0600

    thymeleaf

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,60 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@ResponseBody
+@Controller
+@RequestMapping("hello")
+public class HelloController {
+
+    // handles request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring!";
+//    }
+
+    // now lives at /hello/goodbye
+    @GetMapping("goodbye")
+    public String goodbye(){
+        return "goodbye, Spring!";
+    }
+
+    //handles request of the form /hello?name=LaunchCode
+
+//    @GetMapping("hello")
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryPram(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
+
+    // handles requests of the form /hello/LaunchCode
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name){
+    return "Hello, " + name + "!";
+    }
+
+    // lives at /hello/form
+    @GetMapping("form")
+    public String helloForm(){
+        String html = "<html>" +
+                "<body>" +
+                "<form action='/hello'>" + // submits request to /hello
+                "<input type='text' name='name'/>" +
+                "<input type='submit' value='Greet me!'/>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+        return html;
+    }
+
+
+
+
+    // responds to get and post requests at "/hellogoodbye"
+//    @RequestMapping(value="hellogoodbye", method = {RequestMethod.GET, RequestMethod.POST})
+//    public String hellogoodbye() {
+//        // method code here ...
+//    }
+}
